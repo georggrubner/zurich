6ac48f7477d9d64402ada5f3b5853e91de687ffb
Georg Grubner <grubnergeorg@gmail.com>
2022-11-12 02:38:03 +0100

    installed react-query and fetched the necessary chat data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
+import * as React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+export default ({ Component, pageProps }: AppProps) => {
+    const [queryClient] = React.useState(() => new QueryClient())
+    return (
+        // Provide the client to your App
+        <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    )
 }

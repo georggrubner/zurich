8594bf2774389cae29dfadb658238fd0189dd7d1
georggrubner <grubnergeorg@gmail.com>
2022-11-15 17:12:11 +0100

    added theme compatibility.
    the page will now be displayed in dark or light theme, depending on the users system preference. source: https://mui.com/material-ui/customization/dark-mode/#system-preference

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import CssBaseline from '@mui/material/CssBaseline'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import '@fontsource/roboto/300.css'
@@ -10,11 +13,25 @@ import '@fontsource/roboto/700.css'
 
 export default ({ Component, pageProps }: AppProps) => {
     const [queryClient] = React.useState(() => new QueryClient())
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    )
+
     return (
-        // Provide the client to your App
-        <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
+        <ThemeProvider theme={theme}>
+            <QueryClientProvider client={queryClient}>
+                <CssBaseline enableColorScheme />
+                <Component {...pageProps} />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </ThemeProvider>
     )
 }

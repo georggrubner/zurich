5c20f64bb5d939b346ef02ed2ca308ee8a5c1436
georggrubner <grubnergeorg@gmail.com>
2022-11-15 17:26:51 +0100

    bootstraped general layout of survey

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
+import Stack from '@mui/material/Stack'
+import FormControl from '@mui/material/FormControl'
+import FormLabel from '@mui/material/FormLabel'
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
 
 export default ({ ...props }) => {
     const { isLoading, error, data } = useQuery({
@@ -10,5 +15,21 @@ export default ({ ...props }) => {
         return <div>Loading Data...</div>
     }
 
-    return <div>{JSON.stringify(data)}</div>
+    return (
+        <Stack spacing={5} alignItems="center" justifyContent="center" sx={{ m: 10 }}>
+            <Stack>
+                <div>previous answer1</div>
+                <div>previous answer2</div>
+            </Stack>
+            <FormControl>
+                <Stack spacing={2}>
+                    <FormLabel htmlFor="buttongroup">Lange lange lange lange lange lange lange Frage?</FormLabel>
+                    <ButtonGroup id="buttongroup" sx={{ justifyContent: 'center' }} aria-labelledby="buttongroup">
+                        <Button>One</Button>
+                        <Button>Two</Button>
+                    </ButtonGroup>
+                </Stack>
+            </FormControl>
+        </Stack>
+    )
 }

9e6c4fb9f3d4cf7750f2937e0efeb89aa907e94f
georggrubner <grubnergeorg@gmail.com>
2022-11-15 17:38:12 +0100

    bootstraped react reducer to hold the answers of the questionee

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import { useQuery } from '@tanstack/react-query'
 import Stack from '@mui/material/Stack'
 import FormControl from '@mui/material/FormControl'
@@ -5,11 +6,37 @@ import FormLabel from '@mui/material/FormLabel'
 import Button from '@mui/material/Button'
 import ButtonGroup from '@mui/material/ButtonGroup'
 
+type Action = { type: 'reset' }
+
+export const initialState = {
+    nextId: 100,
+    answers: {},
+}
+
+type State = {
+    nextId: number
+    answers: {
+        [key: number]: {
+            name: string
+            value: string | number | boolean
+        }
+    }
+}
+
+export const reducer = (state: State, action: Action): State => {
+    switch (action.type) {
+        case 'reset': {
+            return initialState
+        }
+    }
+}
+
 export default ({ ...props }) => {
     const { isLoading, error, data } = useQuery({
         queryKey: ['getChatData'],
         queryFn: () => fetch('https://raw.githubusercontent.com/mzronek/task/main/flow.json').then((res) => res.json()),
     })
+    const [{ nextId, answers }, dispatch] = React.useReducer(reducer, initialState)
 
     if (isLoading) {
         return <div>Loading Data...</div>
